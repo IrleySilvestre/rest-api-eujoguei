443fb797d5ee09c845c62d065389e55a6616474f
IrleySilvestre <irleysilvestre.mba@gmail.com>
2022-04-25 12:21:48 -0300

    modifier methods express

--- config/customExpress.js
+++ config/customExpress.js
@@ -4,38 +4,38 @@ const cors = require("cors");
 const consign = require("consign");
 
 var corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
 };
 
 module.exports = () => {
-  const app = express();
+    const app = express();
 
-  // app.use((req, res, next) => {
-  //   res.header("Access-Control-Allow-Origin", "*");
-  //   res.header(
-  //     "Access-Control-Allow-Methods",
-  //     "GET,POS,DELETE,UPDATE,PUT,PATCH"
-  //   );
-  //   res.header("Access-Control-Allow-Headers", [
-  //     "Content-Type",
-  //     "Authorization",
-  //   ]);
+    // app.use((req, res, next) => {
+    //   res.header("Access-Control-Allow-Origin", "*");
+    //   res.header(
+    //     "Access-Control-Allow-Methods",
+    //     "GET,POS,DELETE,UPDATE,PUT,PATCH"
+    //   );
+    //   res.header("Access-Control-Allow-Headers", [
+    //     "Content-Type",
+    //     "Authorization",
+    //   ]);
 
-  //   next();
-  // });
-  app.use(cors(corsOptions));
+    //   next();
+    // });
+    app.use(cors(corsOptions));
 
-  app.use(express.json());
+    app.use(express.json());
 
-  app.use(express.urlencoded({ extended: true }));
+    app.use(express.urlencoded({ extended: true }));
 
-  consign().include("routes").into(app);
+    consign().include("routes").into(app);
 
-  app.get("/", (req, res) => {
-    res.json({ message: "Welcome to eujoguei" });
-  });
+    app.get("/", (req, res) => {
+        res.json({ message: "Welcome to eujoguei" });
+    });
 
-  return app;
+    return app;
 };
